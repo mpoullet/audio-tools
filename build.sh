af3c925c56686a6efc17e4f0450fbb633c28d9a7
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-22 14:42:23 +0200

    Run make_sine again and fix cppcheck.

--- build.sh
+++ build.sh
@@ -11,4 +11,7 @@ clang -std=c11 -o complex_valued_fwd_fft_floats -Wall -Wextra -Werror -pedantic 
 gcc   -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic -I kiss_fft130/tools/ -I kiss_fft130 real_valued_fwd_fft_floats.c    kiss_fft130/kiss_fft.c kiss_fft130/tools/kiss_fftr.c -lm
 clang -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic -I kiss_fft130/tools/ -I kiss_fft130 real_valued_fwd_fft_floats.c    kiss_fft130/kiss_fft.c kiss_fft130/tools/kiss_fftr.c -lm
 
-cppcheck --enable=all *.cc *.c
+cppcheck --enable=all --suppress=missingIncludeSystem *.cc *.c
+
+echo "Running make_sine..."
+./make_sine
