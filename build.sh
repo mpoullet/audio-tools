8ff3b3bfd203b1b13eb39c3add8aeeb2b53af6f8
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-22 14:36:19 +0200

    Rework build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-clang++-3.9 -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine_clang make_sine.cc -lsndfile -lm && ./make_sine_clang
-g++         -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine_g++   make_sine.cc -lsndfile -lm && ./make_sine_g++
-cppcheck --enable=all make_sine.cc
+
+set -e
+set -u
+
+g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine make_sine.cc -lsndfile -lm
+clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine make_sine.cc -lsndfile -lm
+
+gcc   -std=c11 -o complex_valued_fwd_fft_floats -Wall -Wextra -Werror -pedantic -I kiss_fft130/                      complex_valued_fwd_fft_floats.c kiss_fft130/kiss_fft.c                               -lm
+clang -std=c11 -o complex_valued_fwd_fft_floats -Wall -Wextra -Werror -pedantic -I kiss_fft130/                      complex_valued_fwd_fft_floats.c kiss_fft130/kiss_fft.c                               -lm
+gcc   -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic -I kiss_fft130/tools/ -I kiss_fft130 real_valued_fwd_fft_floats.c    kiss_fft130/kiss_fft.c kiss_fft130/tools/kiss_fftr.c -lm
+clang -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic -I kiss_fft130/tools/ -I kiss_fft130 real_valued_fwd_fft_floats.c    kiss_fft130/kiss_fft.c kiss_fft130/tools/kiss_fftr.c -lm
+
+cppcheck --enable=all *.cc *.c
