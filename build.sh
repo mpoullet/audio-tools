a795918cc231b17ca3cf10884138ec4c3b6ea0a8
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-22 11:09:27 +0200

    Run cppcheck.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 clang++-3.9 -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine_clang make_sine.cc -lsndfile -lm && ./make_sine_clang
 g++         -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine_g++   make_sine.cc -lsndfile -lm && ./make_sine_g++
+cppcheck --enable=all make_sine.cc
