e27ab74a0107f21abf1c921ec565839f82229b22
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-24 15:00:05 +0200

    Test command_line executable.

--- build.sh
+++ build.sh
@@ -13,9 +13,9 @@ clang -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic 
 
 cppcheck --enable=all --suppress=missingIncludeSystem *.cc *.c
 
-g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options
-clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options
+g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options -lboost_system -lboost_filesystem
+clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options -lboost_system -lboost_filesystem
 
 echo "Running make_sine..."
 ./make_sine
-./command_line --sr 48000
+./command_line --frequency 440 --sample-rate 48000 --audio-format pcm32 --prefix-name sine
