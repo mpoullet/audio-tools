47d857dc8b539f55730bafeb98e11d7ade7c3078
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-27 23:19:34 +0200

    First try to implement the algorithm.

--- block_processing.cc
+++ block_processing.cc
@@ -20,14 +20,19 @@ int main()
     kiss_fft_cfg inv = kiss_fft_alloc(M, 1, NULL, NULL);
 
     const std::string infilename = "sine_48000_pcm32.wav";
-    const std::string outfilename = "out.wav";
+    const std::string outfilename = "orig.wav";
+    const std::string resfilename = "res.wav";
     SndfileHandle  infile(infilename);
-    SndfileHandle outfile(outfilename, SFM_WRITE, infile.format(), infile.channels(), infile.samplerate() * I/D);
+    SndfileHandle outfile(outfilename, SFM_WRITE, infile.format(), infile.channels(), infile.samplerate());
+    SndfileHandle resfile(resfilename, SFM_WRITE, infile.format(), infile.channels(), infile.samplerate() * I/D);
 
     std::vector<double> buffer(N);
 
     std::vector<std::complex<double>> fwd_fft_in_buffer(N);
     std::vector<std::complex<double>> fwd_fft_out_buffer(N);
+
+    std::vector<std::complex<double>> bwd_fft_in_buffer(M);
+    std::vector<std::complex<double>> bwd_fft_out_buffer(M);
 
     /* prepend 2L zeros */
     for (int i=0; i < 2*L; ++i) {
@@ -53,8 +58,29 @@ int main()
         }
 
         /* Create IFFT input buffer */
+        for(int i=0; i < N/2; ++i) {
+            bwd_fft_in_buffer[i] = static_cast<double>(I/D) * fwd_fft_out_buffer[i];
+        }
+        for(int i=N/2; i <= M - N/2; ++i) {
+            bwd_fft_in_buffer[i] = static_cast<double>(I/D) * std::complex<double>(0.0, 0.0);
+        }
+        for(int i=M - N/2 + 1; i < M; ++i) {
+            bwd_fft_in_buffer[i] = static_cast<double>(I/D) * fwd_fft_out_buffer[i + M/2];
+        }
 
         /* IFFT */
+        kiss_fft(inv, (kiss_fft_cpx *)bwd_fft_in_buffer.data(), (kiss_fft_cpx *)(bwd_fft_out_buffer.data()));
+        for(int i=0; i < M; ++i) {
+            std::cout << "IFFT: " << i << " " << bwd_fft_out_buffer[i] << "\n";
+        }
+
+        /* Discard first and last L points and store the rest of the real vector */
+        std::vector<double> res(M - 2*L);
+        int j = L;
+        for(int i=0; i < M - 2*L; ++i) {
+            res[i] = bwd_fft_out_buffer[j++].real();
+        }
+        resfile.write(res.data(), res.size());
 
         /* Shift vector to the left 2*L times */
         std::rotate(buffer.begin(), buffer.begin() + N - 2*L, buffer.end());
