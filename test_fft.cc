541ef6fdd8d7dfb80634c576402d9b7e88cead33
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-05-30 17:14:12 +0200

    Conversion to C++ done.

--- test_fft.cc
+++ test_fft.cc
@@ -1,8 +1,13 @@
 // Taken from: http://stackoverflow.com/a/14537855
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <complex>
+#include <numeric>
+#include <array>
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979324
@@ -10,49 +15,34 @@
 
 #define N 16
 
-#define kiss_fft_scalar double
-#include "kiss_fftr.h"
+#define kiss_fft_scalar float
+#include "kiss_fft.h"
 
-void TestFftReal(const char *title, const kiss_fft_scalar in[N],
-                 kiss_fft_cpx out[N / 2 + 1]) {
-  kiss_fftr_cfg cfg;
+int main()
+{
+    std::array<kiss_fft_cpx, N> in;
+    std::array<kiss_fft_cpx, N> out;
 
-  printf("%s\n", title);
-
-  if ((cfg = kiss_fftr_alloc(N, 0 /*is_inverse_fft*/, NULL, NULL)) != NULL) {
-    size_t i;
-
-    kiss_fftr(cfg, in, out);
-    free(cfg);
-
-    for (i = 0; i < N; i++) {
-      printf(" in[%2zu] = %+f    ", i, in[i]);
-      if (i < N / 2 + 1)
-        printf("out[%2zu] = %+f , %+f", i, out[i].r, out[i].i);
-      printf("\n");
+    for (size_t i=0; i < N; ++i) {
+        in[i].r = i;
+        in[i].i = 0.0;
+        //std::cout << in[i].r << "," << in[i].i << "\n";
     }
-  } else {
-    printf("not enough memory?\n");
-    exit(-1);
-  }
-}
 
-int main(void) {
-  kiss_fft_scalar in[N];
-  kiss_fft_cpx out[N / 2 + 1];
-  size_t i;
+    kiss_fft_cfg cfg;
+    if ((cfg = kiss_fft_alloc(N, 0, NULL, NULL)) != NULL) {
+        kiss_fft(cfg, in.data(), out.data());
+        free(cfg);
 
-  for (i = 0; i < N; i++)
-    in[i] = 0;
-  TestFftReal("Zeroes (real)", in, out);
+        for (size_t i = 0; i < N; i++) {
+            printf(" in[%2zu] = %+f , %+f    "
+             "out[%2zu] = %+f , %+f\n",
+             i, in[i].r, in[i].i, i, out[i].r, out[i].i);
+        }
+    } else {
+        printf("not enough memory\n");
+        return -1;
+    }
 
-  for (i = 0; i < N; i++)
-    in[i] = 1;
-  TestFftReal("Ones (real)", in, out);
-
-  for (i = 0; i < N; i++)
-    in[i] = sin(2 * M_PI * 4 * i / N);
-  TestFftReal("SineWave (real)", in, out);
-
-  return 0;
+    return 0;
 }
