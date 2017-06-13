68a1ce979bb41398b4350dca3afa9fcdb001d2ef
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-13 14:31:13 +0200

    Remove Kiss FFT examples.

--- build.sh
+++ build.sh
@@ -7,14 +7,6 @@ set -u
 g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine make_sine.cc -lsndfile -lm
 clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine make_sine.cc -lsndfile -lm
 
-# Kiss FFT examples
-
-## C11
-gcc   -std=c11 -o complex_valued_fwd_fft_floats -Wall -Wextra -Werror -pedantic -I kiss_fft130/                      complex_valued_fwd_fft_floats.c kiss_fft130/kiss_fft.c                               -lm
-clang -std=c11 -o complex_valued_fwd_fft_floats -Wall -Wextra -Werror -pedantic -I kiss_fft130/                      complex_valued_fwd_fft_floats.c kiss_fft130/kiss_fft.c                               -lm
-gcc   -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic -I kiss_fft130/tools/ -I kiss_fft130 real_valued_fwd_fft_floats.c    kiss_fft130/kiss_fft.c kiss_fft130/tools/kiss_fftr.c -lm
-clang -std=c11 -o real_valued_fwd_fft_floats    -Wall -Wextra -Werror -pedantic -I kiss_fft130/tools/ -I kiss_fft130 real_valued_fwd_fft_floats.c    kiss_fft130/kiss_fft.c kiss_fft130/tools/kiss_fftr.c -lm
-
 # commmand_line
 g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options -lboost_system -lboost_filesystem -lsndfile -lm
 clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options -lboost_system -lboost_filesystem -lsndfile -lm
