0336d49547b13354a2f0d3c604cb967a0a8b07e8
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-14 10:33:19 +0200

    Use make to build binaries.

--- build.sh
+++ build.sh
@@ -1,38 +1,17 @@
 #!/bin/sh
 
+#set -x
 set -e
 set -u
 
-# make_sine
-g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine make_sine.cc -lsndfile -lm
-clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o make_sine make_sine.cc -lsndfile -lm
+echo "Building..."
+CC=gcc CXX=g++       make -C KissFFT/examples clean all
+CC=clang CXX=clang++ make -C KissFFT/examples clean all
 
-# commmand_line
-g++     -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options -lboost_system -lboost_filesystem -lsndfile -lm
-clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -o command_line command_line.cc -lboost_program_options -lboost_system -lboost_filesystem -lsndfile -lm
-
-# block_processing
-
-KissFFT_DIR="KissFFT/kiss_fft130"
-
-## GNU
-## Step 1: build Kiss FFT object files
-gcc -std=c11 -Wall -Wextra -Werror -pedantic -I ${KissFFT_DIR}                         -c ${KissFFT_DIR}/kiss_fft.c
-gcc -std=c11 -Wall -Wextra -Werror -pedantic -I ${KissFFT_DIR} -I ${KissFFT_DIR}/tools -c ${KissFFT_DIR}/tools/kiss_fftr.c
-# Step 2: build block_processing
-g++ -std=c++14 -Wall -Wextra -Werror -pedantic -I ${KissFFT_DIR} -I ${KissFFT_DIR}/tools/ -o block_processing kiss_fft.o kiss_fftr.o block_processing.cc -lsndfile
-
-## Clang
-## Step 1: build Kiss FFT object files
-clang -std=c11 -Wall -Wextra -Werror -pedantic -I ${KissFFT_DIR}                         -c ${KissFFT_DIR}/kiss_fft.c
-clang -std=c11 -Wall -Wextra -Werror -pedantic -I ${KissFFT_DIR} -I ${KissFFT_DIR}/tools -c ${KissFFT_DIR}/tools/kiss_fftr.c
-# Step 2: build block_processing
-clang++ -std=c++14 -Wall -Wextra -Werror -pedantic -I ${KissFFT_DIR}/ -I ${KissFFT_DIR}/tools/ -o block_processing kiss_fft.o kiss_fftr.o block_processing.cc -lsndfile
-
-# cppcheck
-cppcheck --enable=all --suppress=missingIncludeSystem *.cc *.c
+CC=gcc CXX=g++       make -C src clean all
+CC=clang CXX=clang++ make -C src clean all
 
 echo "Running..."
 #./make_sine
-./command_line --frequency 440 --sample-rate 48000 --audio-format pcm32 --prefix-name sine --duration 0.00909090 --verbose
-./block_processing
+bin/command_line --frequency 440 --sample-rate 48000 --audio-format pcm32 --prefix-name sine --duration 0.00909090 --verbose
+bin/block_processing
