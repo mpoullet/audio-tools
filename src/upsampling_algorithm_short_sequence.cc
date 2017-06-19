4eea85ec6350a189eb84c4d2c88f0d2a76afe693
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-19 10:38:41 +0200

    Use multiple file handles.

--- src/upsampling_algorithm_short_sequence.cc
+++ src/upsampling_algorithm_short_sequence.cc
@@ -12,16 +12,6 @@
 #define kiss_fft_scalar float
 #include "kiss_fft.h"
 
-// https://stackoverflow.com/a/2284805/496459
-template <typename Stream>
-void reopen(Stream& pStream, const char * pFile,
-            std::ios_base::openmode pMode = std::ios_base::out)
-{
-    pStream.close();
-    pStream.clear();
-    pStream.open(pFile, pMode);
-}
-
 int main(int argc, char *argv[])
 {
     const int I = 2;
@@ -73,14 +63,14 @@ int main(int argc, char *argv[])
         fft_input_buffer[i] = std::complex<kiss_fft_scalar>(buffer[i]);
     }
 
-    std::ofstream ofs("fft_input_buffer.asc");
-    std::copy(fft_input_buffer.begin(), fft_input_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(ofs, "\n"));
+    std::ofstream fft_input_buffer_file("fft_input_buffer.asc");
+    std::copy(fft_input_buffer.begin(), fft_input_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(fft_input_buffer_file, "\n"));
 
     // Forward N points FFT
     kiss_fft(fwd_cfg.get(), reinterpret_cast<kiss_fft_cpx*>(fft_input_buffer.data()), reinterpret_cast<kiss_fft_cpx*>(fft_output_buffer.data()));
 
-    reopen(ofs, "fft_output_buffer.asc");
-    std::copy(fft_output_buffer.begin(), fft_output_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(ofs, "\n"));
+    std::ofstream fft_output_buffer_file("fft_output_buffer.asc");
+    std::copy(fft_output_buffer.begin(), fft_output_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(fft_output_buffer_file, "\n"));
 
     // Create IFFT input buffer
     for (int i=0; i < N/2; ++i) {
@@ -93,16 +83,16 @@ int main(int argc, char *argv[])
         ifft_input_buffer[i] = static_cast<kiss_fft_scalar>(I/D) * fft_output_buffer[i - N];
     }
 
-    reopen(ofs, "ifft_input_buffer.asc");
-    std::copy(ifft_input_buffer.begin(), ifft_input_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(ofs, "\n"));
+    std::ofstream ifft_input_buffer_file("ifft_input_buffer.asc");
+    std::copy(ifft_input_buffer.begin(), ifft_input_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(ifft_input_buffer_file, "\n"));
 
     // Backward M points IFFT
     kiss_fft(inv_cfg.get(), reinterpret_cast<kiss_fft_cpx*>(ifft_input_buffer.data()), reinterpret_cast<kiss_fft_cpx*>(ifft_output_buffer.data()));
     std::transform(ifft_output_buffer.begin(), ifft_output_buffer.end(),
                    ifft_output_buffer.begin(), std::bind1st(std::multiplies<std::complex<float>>(),  1.0/M ));
 
-    reopen(ofs, "ifft_output_buffer.asc");
-    std::copy(ifft_output_buffer.begin(), ifft_output_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(ofs, "\n"));
+    std::ofstream ifft_output_buffer_file("ifft_output_buffer.asc");
+    std::copy(ifft_output_buffer.begin(), ifft_output_buffer.end(), std::ostream_iterator<std::complex<kiss_fft_scalar>>(ifft_output_buffer_file, "\n"));
 
     kiss_fft_cleanup();
     return 0;
