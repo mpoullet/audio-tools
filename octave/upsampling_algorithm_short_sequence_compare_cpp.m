9de882502c2b65129847b0929818d11abe0a2741
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-19 10:21:11 +0200

    Use the read_complex_file function.

--- octave/upsampling_algorithm_short_sequence_compare_cpp.m
+++ octave/upsampling_algorithm_short_sequence_compare_cpp.m
@@ -1,15 +1,11 @@
-fft_input_buffer_values = load('../src/fft_input_buffer.asc');
-fft_input_buffer = complex(fft_input_buffer_values(:,2), fft_input_buffer_values(:,3));
+fft_input_buffer = read_complex_file('../src/fft_input_buffer.asc');
 cmp_fft_input = [fft_input_data, fft_input_buffer];
 
-fft_output_buffer_values = load('../src/fft_output_buffer.asc');
-fft_output_buffer = complex(fft_output_buffer_values(:,2), fft_output_buffer_values(:,3));
+fft_output_buffer = read_complex_file('../src/fft_output_buffer.asc');
 cmp_fft_output = [fft_output_data, fft_output_buffer];
 
-ifft_input_buffer_values = load('../src/ifft_input_buffer.asc');
-ifft_input_buffer = complex(ifft_input_buffer_values(:,2), ifft_input_buffer_values(:,3));
+ifft_input_buffer = read_complex_file('../src/ifft_input_buffer.asc');
 cmp_ifft_input = [ifft_input_data_zeros, ifft_input_buffer];
 
-ifft_output_buffer_values = load('../src/ifft_output_buffer.asc');
-ifft_output_buffer = complex(ifft_output_buffer_values(:,2), ifft_output_buffer_values(:,3));
+ifft_output_buffer = read_complex_file('../src/ifft_output_buffer.asc');
 cmp_ifft_output = [ifft_output_data_zeros, ifft_output_buffer];
