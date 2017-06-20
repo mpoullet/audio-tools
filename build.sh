92cc736cbabe48b93138c4e5212622ee59160fbb
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-20 17:25:19 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -10,18 +10,19 @@ YELLOW='\033[1;33m'
 NOCOLOR='\033[0m'
 
 echo -e "${YELLOW}Building with gcc/g++...${NOCOLOR}"
-CC=gcc   CXX=g++     make -C KissFFT/examples clean all
-CC=clang CXX=clang++ make -C KissFFT/examples clean all
+make -C KissFFT/examples clean all CC=gcc   CXX=g++
+make -C KissFFT/examples clean all CC=clang CXX=clang++
 
 echo -e "${YELLOW}Building with clang/clang++...${NOCOLOR}"
-CC=gcc   CXX=g++     make -C src clean all
-CC=clang CXX=clang++ make -C src clean all
+make -C src clean all CC=gcc   CXX=g++
+make -C src clean all CC=clang CXX=clang++
 
 echo -e "${YELLOW}Checking C/C++ source code...${NOCOLOR}"
 find ./KissFFT/examples/ ./src \( -name "*.c" -o -name "*.cc" \) -exec cppcheck \
     --enable=all \
     --suppress=missingIncludeSystem \
     --check-config \
+    -I src/include \
     -I KissFFT/kiss_fft130/ \
     -I KissFFT/kiss_fft130/tools/ {} +
 
