03696bb1d5ef37dd02dcd7210c1316ceaf3919c1
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-21 14:45:25 +0200

    Add the Octave function plot_wav_file.

---
+++ octave/plot_wav_file.m
@@ -0,0 +1,17 @@
+function [x, y] = plot_wav_file(filename, title_name)
+    % WAV file input
+    [data, fs, nbits] = wavread(filename);
+    sample_counts = length(data);
+
+    % Plot sound file in time domain
+    t_all = (1/fs)*(1:sample_counts);
+    figure('Name', title_name, 'NumberTitle', 'off');
+    plot(t_all, data);
+    xlabel('Time (s)');
+    ylabel('Amplitude');
+    ylim([-1 1]);
+    title(title_name);
+    
+    x = t_all;
+    y = data;
+end
