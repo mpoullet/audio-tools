6acf1abfc373e39de1a4c228c494a934c1afb02a
Matthieu Poullet <matthieu.poullet@gmail.com>
2017-06-23 09:31:09 +0200

    Rename output files.

--- src/make_sine.cc
+++ src/make_sine.cc
@@ -20,22 +20,32 @@ int main() {
     std::cout << std::endl;
 
     std::array<double, sample_count> buffer;
-    for (int k = 0; k < sample_count; k++) {
+    for (int k = 0; k < sample_count; ++k) {
         buffer[k] = sin(freq * 2 * k * pi / samplerate);
         //std::cout << k << ": " << sample_duration*k << ": " << buffer[k] << "\n";
     }
 
-    SndfileHandle sndfilehandle_pcm32("sine32.wav", SFM_WRITE, (SF_FORMAT_WAV | SF_FORMAT_PCM_32), 1, samplerate);
-    SndfileHandle sndfilehandle_pcm24("sine24.wav", SFM_WRITE, (SF_FORMAT_WAV | SF_FORMAT_PCM_24), 1, samplerate);
-    SndfileHandle sndfilehandle_pcm16("sine16.wav", SFM_WRITE, (SF_FORMAT_WAV | SF_FORMAT_PCM_16), 1, samplerate);
-    SndfileHandle sndfilehandle_pcmf ("sinef.wav",  SFM_WRITE, (SF_FORMAT_WAV | SF_FORMAT_FLOAT),  1, samplerate);
-    SndfileHandle sndfilehandle_pcmd ("sined.wav",  SFM_WRITE, (SF_FORMAT_WAV | SF_FORMAT_DOUBLE), 1, samplerate);
+    SndfileHandle sndfilehandle_pcm32("sine_" + std::to_string(freq) + "_" + std::to_string(samplerate) + "_pcm32.wav",
+                                      SFM_WRITE,
+                                      (SF_FORMAT_WAV | SF_FORMAT_PCM_32), 1, samplerate);
+    SndfileHandle sndfilehandle_pcm24("sine_" + std::to_string(freq) + "_" + std::to_string(samplerate) + "_pcm24.wav",
+                                      SFM_WRITE,
+                                      (SF_FORMAT_WAV | SF_FORMAT_PCM_24), 1, samplerate);
+    SndfileHandle sndfilehandle_pcm16("sine_" + std::to_string(freq) + "_" + std::to_string(samplerate) + "_pcm16.wav",
+                                      SFM_WRITE,
+                                      (SF_FORMAT_WAV | SF_FORMAT_PCM_16), 1, samplerate);
+    SndfileHandle sndfilehandle_pcmf("sine_" + std::to_string(freq) + "_" + std::to_string(samplerate) + "_float.wav",
+                                     SFM_WRITE,
+                                     (SF_FORMAT_WAV | SF_FORMAT_FLOAT),  1, samplerate);
+    SndfileHandle sndfilehandle_pcmd("sine_" + std::to_string(freq) + "_" + std::to_string(samplerate) + "_double.wav",
+                                     SFM_WRITE,
+                                     (SF_FORMAT_WAV | SF_FORMAT_DOUBLE), 1, samplerate);
 
-    sndfilehandle_pcm32.write(buffer.data(), sample_count);
-    sndfilehandle_pcm24.write(buffer.data(), sample_count);
-    sndfilehandle_pcm16.write(buffer.data(), sample_count);
-    sndfilehandle_pcmf.write (buffer.data(), sample_count);
-    sndfilehandle_pcmd.write (buffer.data(), sample_count);
+    sndfilehandle_pcm32.write(buffer.data(), buffer.size());
+    sndfilehandle_pcm24.write(buffer.data(), buffer.size());
+    sndfilehandle_pcm16.write(buffer.data(), buffer.size());
+    sndfilehandle_pcmf.write (buffer.data(), buffer.size());
+    sndfilehandle_pcmd.write (buffer.data(), buffer.size());
 
     return 0;
 }
