256e23cea3e6e05741d7ed38a6f00a6cc3e1489d
Matthieu Poullet <matthieu.poullet@gmail.com>
2018-01-10 11:23:22 +0100

    Code cleanup.

--- src/generate_A440.cc
+++ src/generate_A440.cc
@@ -15,13 +15,6 @@ int main ()
     const auto sample_duration{1.0 / samplerate};
     const int sample_count = samplerate * std::round (duration);
 
-    static_assert (std::is_same_v<const double, decltype (pi)>);
-    static_assert (std::is_same_v<const double, decltype (freq)>);
-    static_assert (std::is_same_v<const double, decltype (duration)>);
-    static_assert (std::is_same_v<const int, decltype (samplerate)>);
-    static_assert (std::is_same_v<const double, decltype (sample_duration)>);
-    static_assert (std::is_same_v<const int, decltype (sample_count)>);
-
     std::cout << "duration=" << duration << "\n";
     std::cout << "samplerate=" << samplerate << "\n";
     std::cout << "sample_count=" << sample_count << "\n";
@@ -34,19 +27,20 @@ int main ()
     std::transform (std::begin (buffer), std::end (buffer), std::begin (buffer),
                     [&](auto k) { return sin (freq * 2 * k * pi / samplerate); });
 
-    SndfileHandle sndfilehandle_pcm32 ("sine_" + std::to_string (freq) + "_" + std::to_string (samplerate) + "_pcm32.wav",
+    std::string filename_prefix = "sine_" + std::to_string (freq) + "_" + std::to_string (samplerate);
+    SndfileHandle sndfilehandle_pcm32 (filename_prefix + "_pcm32.wav",
                                        SFM_WRITE,
                                        (SF_FORMAT_WAV | SF_FORMAT_PCM_32), 1, samplerate);
-    SndfileHandle sndfilehandle_pcm24 ("sine_" + std::to_string (freq) + "_" + std::to_string (samplerate) + "_pcm24.wav",
+    SndfileHandle sndfilehandle_pcm24 (filename_prefix + "_pcm24.wav",
                                        SFM_WRITE,
                                        (SF_FORMAT_WAV | SF_FORMAT_PCM_24), 1, samplerate);
-    SndfileHandle sndfilehandle_pcm16 ("sine_" + std::to_string (freq) + "_" + std::to_string (samplerate) + "_pcm16.wav",
+    SndfileHandle sndfilehandle_pcm16 (filename_prefix + "_pcm16.wav",
                                        SFM_WRITE,
                                        (SF_FORMAT_WAV | SF_FORMAT_PCM_16), 1, samplerate);
-    SndfileHandle sndfilehandle_pcmf ("sine_" + std::to_string (freq) + "_" + std::to_string (samplerate) + "_float.wav",
+    SndfileHandle sndfilehandle_pcmf (filename_prefix + "_float.wav",
                                       SFM_WRITE,
                                       (SF_FORMAT_WAV | SF_FORMAT_FLOAT), 1, samplerate);
-    SndfileHandle sndfilehandle_pcmd ("sine_" + std::to_string (freq) + "_" + std::to_string (samplerate) + "_double.wav",
+    SndfileHandle sndfilehandle_pcmd (filename_prefix + "_double.wav",
                                       SFM_WRITE,
                                       (SF_FORMAT_WAV | SF_FORMAT_DOUBLE), 1, samplerate);
 
@@ -55,6 +49,4 @@ int main ()
     sndfilehandle_pcm16.write (buffer.data (), buffer.size ());
     sndfilehandle_pcmf.write (buffer.data (), buffer.size ());
     sndfilehandle_pcmd.write (buffer.data (), buffer.size ());
-
-    return 0;
 }
